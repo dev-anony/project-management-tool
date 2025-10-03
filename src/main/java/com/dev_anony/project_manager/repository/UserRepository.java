97b84018cb5aa65fa8c1898e9f7f8bab2a8adb9a
dev-anony <vs0562944@gmail.com>
2025-10-03 23:04:33 +0530

    Update

---
+++ src/main/java/com/dev_anony/project_manager/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.dev_anony.project_manager.repository;
+
+import com.dev_anony.project_manager.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
+
