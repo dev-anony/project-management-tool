b25e47731deb198ef0ea6f4cc26d73c0784c15ed
dev-anony <vs0562944@gmail.com>
2025-11-06 12:18:06 +0530

    fixed basic errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,4 @@
 import { useState } from 'react'
-<<<<<<< HEAD
-=======
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
->>>>>>> d3242b93a4997c8c79689a90e43cb59cc583c084
 import './App.css'
 import DraggableButton from './components/Board/Card'
 import { DndContext } from '@dnd-kit/core'
