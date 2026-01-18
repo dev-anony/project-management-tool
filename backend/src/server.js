1d8f39c7f30e6a97ab57abf2ad85da4fd58241b1
dev-anony <vs0562944@gmail.com>
2026-01-18 18:57:42 +0530

    Added Middleware dummy

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,14 @@ const app = express();
 connectDB();
 
 //middleware for CORS
+app.use((req, res, next) => {
+    console.log(`${req.method} request for '${req.url}'`);
+    console.log(`${req.socket.remoteAddress}`);
+    console.log("Headers:", req.headers);
+    next();
+});
 
+//middleware to parse JSON bodies
 app.use(express.json());
 
 app.use("/api", router);
