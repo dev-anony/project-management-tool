5b48dccd611e25844c5f43fb745d1427c915d6aa
dev-anony <vs0562944@gmail.com>
2026-06-04 18:58:40 +0530

    added Mailtrap

---
+++ backend/src/config/mailtrap.js
@@ -0,0 +1,31 @@
+import { MailtrapClient } from "mailtrap";
+import dotenv from "dotenv";
+
+dotenv.config({path: "../../.env"});
+
+
+const TOKEN = process.env.MAILTRAP_TOKEN;
+
+const client = new MailtrapClient({
+  token: TOKEN,
+});
+
+const sender = {
+  email: "hello@demomailtrap.co",
+  name: "Mailtrap Test",
+};
+const recipients = [
+  {
+    email: "tyz31935@gmail.com",
+  }
+];
+
+client
+  .send({
+    from: sender,
+    to: recipients,
+    subject: "You are awesome!",
+    text: "Congrats for sending test email with Mailtrap!",
+    category: "Integration Test",
+  })
+  .then(console.log, console.error);
